091b071d46ba7b710929f80753dfdd5f5158237b
minhaz810 <chowdhuryminhaz021@gmail.com>
2025-03-30 10:55:41 +0600

    tab sequence

--- Program2.java
+++ Program2.java
@@ -1,5 +1,6 @@
 public class Program2 {
     public static void main(String[] args){
-        System.out.print("Hello! \n This is a new line");
+        System.out.print("Hello!\nThis is a new line\n");
+        System.out.println("tab1\ttab2");
     }
 }
